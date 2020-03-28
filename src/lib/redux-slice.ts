c7a386637787b4fdc3cfe75949ead242ab291934
Lars Thorup <lars@zealake.com>
2020-03-28 11:52:23 +0100

    refactoring

--- src/lib/redux-slice.ts
+++ src/lib/redux-slice.ts
@@ -9,42 +9,59 @@ export type SliceReducer<TState, TPayload = void> = (
   action: { payload: TPayload } // Note: action always have payload
 ) => TState;
 
+// SliceReducers is used below to restrict the type of a set of reducers in a slice
+type SliceReducers<TState> = {
+  [key: string]: SliceReducer<TState, any>;
+};
+
 // Reducer is used below to type the "reducer" returned by createSlice()
 type Reducer<TState> = (state: TState | undefined, action: AnyAction) => TState;
 
 // SliceConfig can be used to type the parameter to createSlice()
-type SliceConfig<
-  TState,
-  TSliceReducers extends {
-    [key: string]: SliceReducer<TState, any>;
-  }
-> = {
+type SliceConfig<TState, TSliceReducers extends SliceReducers<TState>> = {
   name: string;
   initialState: TState;
   reducers: TSliceReducers;
 };
 
 // Slice can be used to type the result of createSlice()
-type Slice<
-  TState,
-  TSliceReducers extends {
-    [key: string]: SliceReducer<TState, any>;
-  }
-> = {
+type Slice<TState, TSliceReducers extends SliceReducers<TState>> = {
   name: string;
   actions: Actions<TState, TSliceReducers>;
   reducer: Reducer<TState>;
 };
 
 // createSlice transforms SliceReducers to Redux-compatible Actions and a Reducer
-export const createSlice = <
-  TState,
-  TSliceReducers extends {
-    [key: string]: SliceReducer<TState, any>;
-  }
->(
+type CreateSlice = <TState, TSliceReducers extends SliceReducers<TState>>(
   sliceConfig: SliceConfig<TState, TSliceReducers>
-): Slice<TState, TSliceReducers> => {
+) => Slice<TState, TSliceReducers>;
+export const createSlice: CreateSlice = sliceConfig => {
+  const actions = createActions(sliceConfig);
+  const reducer = createReducer(sliceConfig);
+  return {
+    name: sliceConfig.name,
+    actions,
+    reducer
+  };
+};
+
+// Actions is used in Slice to type the "actions" returned by createSlice()
+type Actions<TState, TSliceReducers extends SliceReducers<TState>> = {
+  [Prop in keyof TSliceReducers]: ActionCreator<
+    PayloadParameterTypeOf<TSliceReducers[Prop]>
+  >;
+};
+
+// PayloadParameterTypeOf is used by Actions to extract the type of the "payload" of the "action" parameter of a slice reducer
+type PayloadParameterTypeOf<
+  TSliceReducer extends SliceReducer<any, any>
+> = Parameters<TSliceReducer>[1]['payload'] extends {}
+  ? Parameters<TSliceReducer>[1]['payload']
+  : void;
+
+const createActions = <TState, TSliceReducers extends SliceReducers<any>>(
+  sliceConfig: SliceConfig<TState, TSliceReducers>
+): Actions<TState, TSliceReducers> => {
   const actionCreatorFromReducer = <TPayload>(
     _: any,
     key: string
@@ -55,8 +72,13 @@ export const createSlice = <
   const actions = R.mapObjIndexed(
     actionCreatorFromReducer,
     sliceConfig.reducers
-  ) as Actions<typeof sliceConfig.initialState, typeof sliceConfig.reducers>;
+  ) as Actions<TState, TSliceReducers>;
+  return actions;
+};
 
+const createReducer = <TState, TSliceReducers extends SliceReducers<any>>(
+  sliceConfig: SliceConfig<TState, TSliceReducers>
+): Reducer<TState> => {
   const reducerByType = R.fromPairs(
     R.toPairs(
       sliceConfig.reducers
@@ -78,29 +100,5 @@ export const createSlice = <
       return state;
     }
   };
-
-  return {
-    name: sliceConfig.name,
-    actions,
-    reducer
-  };
+  return reducer;
 };
-
-// Actions is used in Slice to type the "actions" returned by createSlice()
-type Actions<
-  TState,
-  TSliceReducers extends {
-    [key: string]: SliceReducer<TState, any>;
-  }
-> = {
-  [Prop in keyof TSliceReducers]: ActionCreator<
-    PayloadParameterTypeOf<TSliceReducers[Prop]>
-  >;
-};
-
-// PayloadParameterTypeOf is used by Actions to extract the type of the "payload" of the "action" parameter of a slice reducer
-type PayloadParameterTypeOf<
-  TSliceReducer extends SliceReducer<any, any>
-> = Parameters<TSliceReducer>[1]['payload'] extends {}
-  ? Parameters<TSliceReducer>[1]['payload']
-  : void;
