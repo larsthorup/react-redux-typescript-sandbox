1cd58e7a69b48f5bd12530f5635b0cc2064d5e7f
Lars Thorup <lars@zealake.com>
2020-03-28 10:11:20 +0100

    redux-slice: test

---
+++ src/lib/redux-slice.test.ts
@@ -0,0 +1,54 @@
+import * as Redux from 'redux';
+import { createSlice, Reducer } from '../lib/redux-slice';
+
+type User = {
+  name: string;
+};
+
+type AuthState = Readonly<{
+  user: User | null;
+}>;
+
+test('createSlice', () => {
+  // Given a store with a slice
+  const initialState = {
+    user: null
+  } as AuthState;
+  const signin: Reducer<AuthState, { user: User }> = (
+    state,
+    { payload: { user } }
+  ) => {
+    return { ...state, user };
+  };
+  const signout: Reducer<AuthState> = state => {
+    return { ...state, user: null };
+  };
+  const slice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+      signin,
+      signout
+    }
+  });
+  const rootReducer = Redux.combineReducers({
+    auth: slice.reducer
+  });
+  const store = Redux.createStore(rootReducer);
+  const { dispatch, getState } = store;
+
+  // Initially, slice state is initial state
+  expect(getState().auth.user).toBeNull();
+
+  // When dispatching an action
+  dispatch(slice.actions.signin({ user: { name: 'Lars' } }));
+
+  // Then, slice state has updated accordingly
+  expect(getState().auth.user).toEqual({ name: 'Lars' });
+
+  // When dispatching another action
+  dispatch(slice.actions.signout());
+
+  // Then, slice state has updated accordingly
+  expect(getState().auth.user).toBeNull();
+});
