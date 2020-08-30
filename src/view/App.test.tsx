9a90337f3b32bcbfed52dcc6e5c89917012b8454
Lars Thorup <lars@zealake.com>
2020-08-30 15:42:33 +0200

    Table: row selection

--- src/view/App.test.tsx
+++ src/view/App.test.tsx
@@ -52,6 +52,41 @@ test('auth flow', async () => {
   expect(getRowRenderCount()).toEqual(4);
   resetRowRenderCount();
 
+  // Then: number of selected people is 0
+  expect(
+    screen
+      .getAllByRole('checkbox')
+      .filter((cb) => (cb as HTMLInputElement).checked)
+  ).toHaveLength(0);
+
+  // When: select first person
+  userEvent.click(screen.getAllByRole('checkbox')[1]); // Note: skipping header checkbox
+
+  // Then: number of selected people is 1
+  expect(
+    screen
+      .getAllByRole('checkbox')
+      .filter((cb) => (cb as HTMLInputElement).checked)
+  ).toHaveLength(1);
+
+  // Then: just that row is re-rendered
+  expect(getRowRenderCount()).toEqual(1);
+  resetRowRenderCount();
+
+  // When: select all people
+  userEvent.click(screen.getAllByRole('checkbox')[0]); // Note: header checkbox
+
+  // Then: number of selected people is 4
+  expect(
+    screen
+      .getAllByRole('checkbox')
+      .filter((cb) => (cb as HTMLInputElement).checked)
+  ).toHaveLength(5); // Note: including header checkbox
+
+  // Then: all rows are re-rendered
+  expect(getRowRenderCount()).toEqual(4);
+  resetRowRenderCount();
+
   // When: click first edit button
   userEvent.click(screen.getByRole('button', { name: 'Edit Adam' }));
 
